cdffd63b9e438e33121005be196864f8b670b282
Allan Joston Fernandes <allanfernandes459@gmail.com>
2024-12-15 16:05:46 +0530

    initial setup

---
+++ app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Montivo: Revolutionize Your Video Creation with AI-Powered Editing Tools",
+  description: "Montivo is the ultimate AI-driven video editor, offering seamless video generation, precise editing, and professional-grade tools. Bring your stories to life with effortless creativity.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
