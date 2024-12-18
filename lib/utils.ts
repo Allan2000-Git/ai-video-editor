99ac9d240297371e923be30338de0e305fa4ec5f
Allan Joston Fernandes <allanfernandes459@gmail.com>
2024-12-19 00:36:47 +0530

    added function to generate initials of a name

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,12 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function generateInitials(firstName: string | null, lastName: string | null) {
+  if (!firstName || !lastName) return '';
+
+  const firstInitial = firstName.charAt(0).toUpperCase();
+  const lastInitial = lastName.charAt(0).toUpperCase();
+
+  return `${firstInitial}${lastInitial}`;
+}
