f710f3bf27814d61c0b7d652534f0e8f9179a50c
Allan Joston Fernandes <allanfernandes459@gmail.com>
2024-12-22 17:18:12 +0530

    feat(context): setup remotion video player context

---
+++ contexts/remotion-player-context.tsx
@@ -0,0 +1,37 @@
+import { createContext, Dispatch, SetStateAction, useContext, useState } from "react";
+
+interface RemotionPlayerType {
+    videoFrames: VideoFramesType;
+    setVideoFrames: Dispatch<SetStateAction<VideoFramesType>>
+}
+
+interface VideoFramesType {
+  frames: any[];
+  totalDuration: number;
+  selectedFrame: number;
+}
+
+export const RemotionPlayerContext = createContext<RemotionPlayerType | undefined>(undefined);
+
+export function RemotionPlayerContextProvider({children}: {children: React.ReactNode}) {
+    const [videoFrames, setVideoFrames] = useState<VideoFramesType>({
+      frames: [],
+      totalDuration: 2,
+      selectedFrame: 0
+    });
+
+    return (
+      //giving access globally
+      <RemotionPlayerContext.Provider value={{videoFrames, setVideoFrames}}>
+        {children}
+      </RemotionPlayerContext.Provider>
+    )
+  }
+
+export const useRemotionPlayerContext = () =>{
+  const context = useContext(RemotionPlayerContext);
+  if (context === undefined) {
+    throw new Error("useRemotionPlayerContext must be used within an RemotionPlayerContextProvider");
+  }
+  return context;
+}
