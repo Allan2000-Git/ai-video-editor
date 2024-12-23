f398fc5871c4d67264a8467d57c1f8e58c9d47ff
Allan Joston Fernandes <allanfernandes459@gmail.com>
2024-12-23 23:42:41 +0530

    feat(schema): added videos schema

--- db/schema.ts
+++ db/schema.ts
@@ -1,5 +1,5 @@
 import { InferSelectModel } from "drizzle-orm";
-import { integer, pgTable, timestamp, varchar, } from "drizzle-orm/pg-core";
+import { index, integer, json, pgEnum, pgTable, timestamp, varchar, } from "drizzle-orm/pg-core";
 
 export const usersTable = pgTable("users", {
     id: integer().primaryKey().generatedAlwaysAsIdentity(),
@@ -12,4 +12,20 @@ export const usersTable = pgTable("users", {
     updatedAt: timestamp().$onUpdate(() => new Date())
 });
 
+export const videoType = pgEnum('type', ['CUSTOM', 'AI']);
+
+export const videosTable = pgTable("videos", {
+    id: integer().primaryKey().generatedAlwaysAsIdentity(),
+    title: varchar({ length: 255 }).notNull(),
+    data: json(),
+    type: videoType().default("CUSTOM").notNull(),
+    createdBy: integer('createdBy').references(() => usersTable.id, {onDelete: 'cascade'}).notNull(),
+    createdAt: timestamp().defaultNow().notNull(),
+}, (table) => [
+    index('created_by_idx').on(table.createdBy)
+]);
+
 export type User = InferSelectModel<typeof usersTable>;
+export type Video = InferSelectModel<typeof videosTable>;
+
+export const VideoTypeValues = videoType.enumValues;
